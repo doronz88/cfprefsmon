93eab4da6071bbcaa921cfca2b9b0acd5d5a8c56
doron zarhi <doron88@gmail.com>
2022-06-29 15:52:50 +0300

    setup: remove package_data to allow global installs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 BASE_DIR = os.path.realpath(os.path.dirname(__file__))
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 PACKAGE_NAME = 'cfprefsmon'
 PACKAGES = [p for p in find_packages() if not p.startswith('tests')]
 
@@ -32,8 +32,6 @@ if __name__ == '__main__':
         long_description_content_type='text/markdown',
         cmdclass={},
         packages=PACKAGES,
-        package_data={'': ['*.txt', '*.TXT', '*.json'], },
-        data_files=[('.', ['requirements.txt'])],
         author='DoronZ',
         install_requires=parse_requirements(),
         entry_points={
