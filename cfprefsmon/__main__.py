d1f44fd0c6cccedd8c5615612cf2a82400702ff5
DoronZ doron88@gmail.com <doron88@gmail.com>
2023-09-10 13:47:12 +0300

    add macos support

--- cfprefsmon/__main__.py
+++ cfprefsmon/__main__.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 import click
-from pymobiledevice3.lockdown import create_using_usbmux
+from maclog.log import get_logger
+from pymobiledevice3.cli.cli_common import LockdownCommand
+from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.services.os_trace import OsTraceService
 
 FORMAT = 'CFPreference[{domain}][{user}][{key}] = {value}  # Process: {procname}'
@@ -13,71 +17,90 @@ USER_PREFIX = ', User: '
 
 DEFAULT_USER = 'kCFPreferencesAnyUser'
 
+PREFS = {}
 
-@click.command()
-@click.option('--udid')
+
+def print_entry(message: str, filename: str, subsystem: Optional[str] = None, category: Optional[str] = None,
+                unique: bool = False, color: bool = False, undefined: bool = False) -> None:
+    if subsystem != 'com.apple.defaults' or category != 'User Defaults':
+        return
+
+    if 'cfprefs' not in message.lower():
+        return
+
+    if not message.startswith(HAS_VALUE_PREFIX) and not message.startswith(NO_VALUE_PREFIX):
+        return
+
+    # print(message)
+    user = DEFAULT_USER
+    value = None
+
+    key = message.split(FOR_KEY_PREFIX, 1)[1].split(FOR_KEY_SUFFIX, 1)[0].strip()
+    domain = message.rsplit(DOMAIN_PREFIX, 1)[1].split(',', 1)[0].strip()
+    procname = filename
+    has_value = False
+
+    if USER_PREFIX in message:
+        user = message.rsplit(USER_PREFIX, 1)[1].split(',', 1)[0].strip()
+
+    if not user:
+        user = DEFAULT_USER
+
+    if message.startswith(HAS_VALUE_PREFIX):
+        value = message.split(HAS_VALUE_PREFIX, 1)[1].split(FOR_KEY_PREFIX, 1)[0]
+        has_value = True
+
+    if domain not in PREFS:
+        PREFS[domain] = {}
+
+    if user not in PREFS[domain]:
+        PREFS[domain][user] = []
+
+    if unique and key in PREFS[domain][user]:
+        return
+
+    PREFS[domain][user].append(key)
+
+    if color:
+        domain = click.style(domain, fg='yellow')
+        user = click.style(user, fg='bright_green')
+        key = click.style(key, fg='green')
+        procname = click.style(procname, fg='magenta')
+
+        if not has_value:
+            value = click.style(value, fg='red')
+
+    if (not undefined) or (undefined and not has_value):
+        print(FORMAT.format(domain=domain, user=user, key=key, value=value, procname=procname))
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option('--unique', is_flag=True, help='output only unique entries')
 @click.option('--color/--no-color', default=True, help='make colored output')
 @click.option('--undefined', is_flag=True, help='filter only non-existing keys')
-def cli(udid, unique, color, undefined):
-    lockdown = create_using_usbmux(serial=udid)
-    prefs = {}
-    for entry in OsTraceService(lockdown).syslog():
+def host(unique, color, undefined):
+    """ Sniff on macOS host """
+    for entry in get_logger():
+        print_entry(entry.event_message, entry.process_image_path, entry.subsystem, entry.category, unique=unique,
+                    color=color, undefined=undefined)
+
+
+@cli.command(cls=LockdownCommand)
+@click.option('--unique', is_flag=True, help='output only unique entries')
+@click.option('--color/--no-color', default=True, help='make colored output')
+@click.option('--undefined', is_flag=True, help='filter only non-existing keys')
+def mobile(service_provider: LockdownClient, unique, color, undefined):
+    """ Sniff on connected iOS device """
+    for entry in OsTraceService(service_provider).syslog():
         if entry.label is None:
             continue
-
-        if entry.label.subsystem != 'com.apple.defaults' or entry.label.category != 'User Defaults':
-            continue
-
-        message = entry.message
-
-        if 'cfprefs' not in message.lower():
-            continue
-
-        if not message.startswith(HAS_VALUE_PREFIX) and not message.startswith(NO_VALUE_PREFIX):
-            continue
-
-        # print(message)
-        user = DEFAULT_USER
-        value = None
-
-        key = message.split(FOR_KEY_PREFIX, 1)[1].split(FOR_KEY_SUFFIX, 1)[0].strip()
-        domain = message.rsplit(DOMAIN_PREFIX, 1)[1].split(',', 1)[0].strip()
-        procname = entry.filename
-        has_value = False
-
-        if USER_PREFIX in message:
-            user = message.rsplit(USER_PREFIX, 1)[1].split(',', 1)[0].strip()
-
-        if not user:
-            user = DEFAULT_USER
-
-        if message.startswith(HAS_VALUE_PREFIX):
-            value = message.split(HAS_VALUE_PREFIX, 1)[1].split(FOR_KEY_PREFIX, 1)[0]
-            has_value = True
-
-        if domain not in prefs:
-            prefs[domain] = {}
-
-        if user not in prefs[domain]:
-            prefs[domain][user] = []
-
-        if unique and key in prefs[domain][user]:
-            continue
-
-        prefs[domain][user].append(key)
-
-        if color:
-            domain = click.style(domain, fg='yellow')
-            user = click.style(user, fg='bright_green')
-            key = click.style(key, fg='green')
-            procname = click.style(procname, fg='magenta')
-
-            if not has_value:
-                value = click.style(value, fg='red')
-
-        if (not undefined) or (undefined and not has_value):
-            print(FORMAT.format(domain=domain, user=user, key=key, value=value, procname=procname))
+        print_entry(entry.message, entry.filename, entry.label.subsystem, entry.label.category, unique=unique,
+                    color=color, undefined=undefined)
 
 
 if __name__ == '__main__':
